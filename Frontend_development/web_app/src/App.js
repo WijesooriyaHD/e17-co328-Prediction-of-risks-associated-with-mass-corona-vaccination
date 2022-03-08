41e1744a1f9eb3afe9e4713f72142f630a97b7bf
WijesooriyaHD <e17407@eng.pdn.ac.lk>
2022-03-08 14:37:49 +0530

    update App.js

--- Frontend_development/web_app/src/App.js
+++ Frontend_development/web_app/src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import Form from './Pages/form';
+import Home from './Pages/home';
+import {Route,Routes , Link} from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Routes>
+      <Route exact path="/" element={ <Home /> }/>
+      <Route path="/form" element={ <Form /> }/>
+      </Routes>
+
     </div>
+    
+
+
+    
+
   );
 }
 
