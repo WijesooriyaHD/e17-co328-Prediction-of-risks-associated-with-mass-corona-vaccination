3395acc0f5f72e19ffe20825512cab0d00df1abd
WijesooriyaHD <e17407@eng.pdn.ac.lk>
2022-03-14 09:25:14 +0530

    update .sql script changed data type float->varchar

--- Backend Development/Database/Risk_Predictor.sql
+++ Backend Development/Database/Risk_Predictor.sql
@@ -11,25 +11,25 @@ gender varchar(8),
 vaccine_type varchar(20),
 living_area varchar(20),
 
-Swelling float,
-Redness float,
-Itching float,
-Fever float,
- Headache float,
- Muscle_Pain float,
- Tiredness float,
- Coughing float,
- Diarrhea float,
- Nausea_and_vomiting float ,
- Breathlessness float,
- Joint_Pain float,
-  Fainted float,
-  Anaphylactic_Reaction float,
-  Tingling float,
-  Swollen_Lymph_Nodes float ,
+Swelling varchar(3),
+Redness varchar(3),
+Itching varchar(3),
+Fever varchar(3),
+ Headache varchar(3),
+ Muscle_Pain varchar(3),
+ Tiredness varchar(3),
+ Coughing varchar(3),
+ Diarrhea varchar(3),
+ Nausea_and_vomiting varchar(3) ,
+ Breathlessness varchar(3),
+ Joint_Pain varchar(3),
+  Fainted varchar(3),
+  Anaphylactic_Reaction varchar(3),
+  Tingling varchar(3),
+  Swollen_Lymph_Nodes varchar(3) ,
  
 PRIMARY KEY (user_id)
 
 );
 
-select * from user 
+select * from user ;
